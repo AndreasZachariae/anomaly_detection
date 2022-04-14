23f4524030f099da03d7ea2f5c30e91005142f25
Andreas Zachariae <andreas.zachariae@hs-karlsruhe.de>
2022-04-14 13:53:21 +0200

    Add empty file structure

---
+++ classification/bag_of_visual_words.py
@@ -0,0 +1,12 @@
+class BagOfVisualWords():
+    def __init__(self):
+        print("")
+
+def main():
+    print("BagOfVisualWords started")
+    
+
+if __name__ == '__main__':
+    main()
+    
+        
---
+++ preprocessing/data_augmentation.py
@@ -0,0 +1,10 @@
+class DataAugmentation():
+    def __init__(self):
+        print("")
+
+def main():
+    print("DataAugmentation started")
+    
+
+if __name__ == '__main__':
+    main()
