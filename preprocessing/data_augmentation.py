d7d5281b3a4fc38abdfa45c70e2c8ddf55f7aa30
Andreas Zachariae <you@example.com>
2022-04-29 17:00:12 +0200

    Add data_augmentation for whole dataset

--- preprocessing/data_augmentation.py
+++ preprocessing/data_augmentation.py
@@ -1,32 +1,64 @@
 import cv2
 import numpy as np
 import random
+import os
+import math
+
 
 class DataAugmentation():
-    def __init__(self):
+    def __init__(self, folder_path, multiplier):
+        self.folder_path = folder_path
+        self.multiplier = multiplier
 
-        self.folder_path = r"dataset/mvtec_anomaly_detection/bottle/test/broken_large"
-        self.dest_path = r"dataset/augment_test"
+    def augment_dataset(self):
+        for type_name in os.listdir(self.folder_path):
+            # only use bottle dataset. remove this if all types should be copied
+            if not type_name == "bottle":
+                continue
 
-    def augment_image(self, image_name):
-        original_image = cv2.imread(self.folder_path + "/" + image_name)
+            for class_name in os.listdir(os.path.join(self.folder_path, type_name, "ground_truth")):
+                for image_name in os.listdir(os.path.join(self.folder_path, type_name, "images", class_name)):
+                    mask_name = image_name.split('.')[0] + "_mask." + image_name.split('.')[1]
+                    mask_path = os.path.join(self.folder_path, type_name, "ground_truth", class_name)
+                    image_path = os.path.join(self.folder_path, type_name, "images", class_name)
 
-        # image=self.horizontal_flip(original_image)
-        # image=self.vertical_flip(original_image)
+                    self.augment_image(image_path, image_name, mask_path, mask_name)
 
-        for rotation in range(10): # np.linspace(start=-90, stop=90, num=10):
-            for brightness in range(5): # np.linspace(start=0.5, stop=1.5, num=5):
-                image, angle = self.rotation(original_image)
-                image, value = self.brightness(original_image)
-                self.save_image(image, image_name.split('.')[0] + "rotate" + str(angle) + "bright" + str(round(value,1)))
+    def augment_image(self, image_path, image_name, mask_path, mask_name):
+        original_image = cv2.imread(os.path.join(image_path, image_name))
+        original_mask = cv2.imread(os.path.join(mask_path, mask_name))
 
-        # self.show_two_images(original_image, image)
+        for flip in ["hflip", "vflip", "noflip"]:
+            if flip == "hflip":
+                image_flipped = self.horizontal_flip(original_image)
+                mask_flipped = self.horizontal_flip(original_mask)
+            if flip == "vflip":
+                image_flipped = self.vertical_flip(original_image)
+                mask_flipped = self.vertical_flip(original_mask)
+            if flip == "noflip":
+                image_flipped = original_image
+                mask_flipped = original_mask
 
-    def show_two_images(self, image_original, image_augmented):
+            for rotation in range(int(math.sqrt(self.multiplier/3))):
+                angle = int(random.uniform(-90, 90))
+                image_rotated = self.rotation(image_flipped, angle)
+                mask_rotated = self.rotation(mask_flipped, angle, mask=True)
+
+                for brightness in range(int(math.sqrt(self.multiplier/3))):
+                    value = random.uniform(0.5, 1.5)
+                    image = self.brightness(image_rotated, value)
+
+                    self.save_image(image, image_path, image_name, flip, angle, value)
+                    self.save_image(mask_rotated, mask_path, mask_name, flip, angle, value)
+
+                    # self.show_three_images(original_image, image, mask_rotated)
+
+    def show_three_images(self, image_original, image_augmented, mask_augmented):
         image_original = cv2.resize(image_original, (0, 0), None, .25, .25)
         image_augmented = cv2.resize(image_augmented, (0, 0), None, .25, .25)
-        vertical_stack = np.hstack((image_original, image_augmented))
-        cv2.imshow("image", vertical_stack)
+        mask_augmented = cv2.resize(mask_augmented, (0, 0), None, .25, .25)
+        image_stack = np.hstack((image_original, image_augmented, mask_augmented))
+        cv2.imshow("image", image_stack)
         cv2.waitKey(0)
 
     def horizontal_flip(self, image):
@@ -35,35 +67,54 @@ class DataAugmentation():
     def vertical_flip(self, image):
         return cv2.flip(image, 0)
 
-    def brightness(self, image, low=0.5, high=1.5):
-        value = random.uniform(low, high)
+    def brightness(self, image, value):
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        hsv = np.array(hsv, dtype = np.float64)
-        hsv[:,:,1] = hsv[:,:,1]*value
-        hsv[:,:,1][hsv[:,:,1]>255]  = 255
-        hsv[:,:,2] = hsv[:,:,2]*value 
-        hsv[:,:,2][hsv[:,:,2]>255]  = 255
-        hsv = np.array(hsv, dtype = np.uint8)
+        hsv = np.array(hsv, dtype=np.float64)
+        hsv[:, :, 1] = hsv[:, :, 1]*value
+        hsv[:, :, 1][hsv[:, :, 1] > 255] = 255
+        hsv[:, :, 2] = hsv[:, :, 2]*value
+        hsv[:, :, 2][hsv[:, :, 2] > 255] = 255
+        hsv = np.array(hsv, dtype=np.uint8)
         image = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        # self.save_image(image, "bright" + str(value))
-        return image, value
+        return image
 
-    def rotation(self, image, angle=90):
-        angle = int(random.uniform(-angle, angle))
+    def rotation(self, image, angle, mask=False):
         h, w = image.shape[:2]
         M = cv2.getRotationMatrix2D((int(w/2), int(h/2)), angle, 1)
-        image = cv2.warpAffine(image, M, (w, h), borderMode=cv2.BORDER_CONSTANT, borderValue=(255,255,255))
-        # self.save_image(image, "rotate" + str(angle))
-        return image, angle
+        if mask:
+            borderValue = (0, 0, 0)
+        else:
+            borderValue = (255, 255, 255)
 
-    def save_image(self, image, name):
-        cv2.imwrite(self.dest_path + "/" + name + ".jpg", image)
+        image = cv2.warpAffine(image,
+                               M,
+                               (w, h),
+                               borderMode=cv2.BORDER_CONSTANT,
+                               borderValue=borderValue)
+        return image
+
+    def save_image(self, image, path, name, flip, angle, value):
+        cv2.imwrite(path + "/" +
+                    name.split('.')[0] +
+                    "_" + flip +
+                    "_rotate" + str(angle) +
+                    "_bright" + str(round(value, 3)) +
+                    "." + name.split('.')[1],
+                    image)
+
 
 def main():
     print("DataAugmentation started")
-    da = DataAugmentation()
-    da.augment_image("000.png")
-    
+
+    folder_path = os.path.join(os.getcwd(), "dataset", "augmented_dataset")
+
+    # The effect of augmentation is an image multiplication by 3*x*x
+    # Only values with the next step in this list have an increasing effect
+    # multiplier = [3, 12, 29, 48, 75, 108, ...]
+
+    da = DataAugmentation(folder_path, multiplier=12)
+    da.augment_dataset()
+
 
 if __name__ == '__main__':
     main()
