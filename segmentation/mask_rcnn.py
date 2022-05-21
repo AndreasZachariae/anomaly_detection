ab00498e818f493365654592eb016dc8e24ca60b
Andreas Zachariae <andreas.zachariae@hs-karlsruhe.de>
2022-05-21 19:25:22 +0200

    Add prediction and visualization function

--- segmentation/mask_rcnn.py
+++ segmentation/mask_rcnn.py
@@ -1,3 +1,4 @@
+import json
 from json.tool import main
 from typing import List
 import requests
@@ -8,18 +9,27 @@ import PIL.Image
 import numpy as np
 import cv2
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 class MaskRCNN():
-    def __init__(self, model_path, only_cpu):
+    def __init__(self, model_path, label_path, type_name, only_cpu):
         if only_cpu:
             os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
             print("Disabled GPU devices")
             
-        self.model = tf.saved_model.load(model_path)
+        # self.model = tf.saved_model.load(model_path)
+        self.labels = self.load_json(label_path, type_name)
         print("Trained model loaded")
+        print("Classes:", self.labels)
+
+    def load_json(self, label_path, type_name):
+        with open(label_path, 'r') as f:
+            json_dict = json.load(f)
+
+        return json_dict[type_name]
         
 
-    def predict(self, image_path):
+    def predict(self, image_path, threshold):
         """Wrapper for Keras Model predict function
 
         Args:
@@ -37,17 +47,106 @@ class MaskRCNN():
 
         # print(list(self.model.signatures.keys()))
         infer = self.model.signatures["serving_default"]
-        print(infer)
-        prediction = infer(tensor)
-        print(prediction["class_predictions_with_background"])
-        print(prediction["detection_classes"])
-        print(prediction["detection_multiclass_scores"])
-        print(prediction["detection_scores"])
-        print(prediction["detection_masks"])
-        print(prediction["num_detections"])
+        # print(infer)
+        predictions = infer(tensor)
+        # print(predictions["detection_boxes"][0][0])              # tf.Tensor([0.1349324  0.15187985 0.68132347 0.885467  ], shape=(4,), dtype=float32)
+        # print(predictions["detection_classes"][0][0])            # tf.Tensor(1.0, shape=(), dtype=float32)
+        # print(predictions["detection_masks"][0][0])              # tf.Tensor([[2.5281906e-03 ... 3.3918718e-06] ...], shape=(33, 33), dtype=float32)
+        # print(predictions["detection_multiclass_scores"][0][0])  # tf.Tensor([8.9492943e-08 9.9990547e-01 5.3700660e-06 8.9032372e-05], shape=(4,), dtype=float32)
+        # print(predictions["detection_scores"][0][0])             # tf.Tensor(0.99990547, shape=(), dtype=float32)
+        # print(predictions["image_shape"])                        # tf.Tensor([1.000e+00 1.024e+03 1.024e+03 3.000e+00], shape=(4,), dtype=float32)
+        # print(predictions["num_detections"])                     # tf.Tensor([100.], shape=(1,), dtype=float32)
+        # print(predictions["num_proposals"])                      # tf.Tensor([300.], shape=(1,), dtype=float32)
 
 
-        return prediction, image
+        scores = predictions["detection_scores"][0].numpy()
+        idx_list = [idx for idx, score in enumerate(scores) if score > threshold]
+
+        detections = []
+
+        for idx in idx_list:
+            detections.append({
+                "class_id": int(predictions["detection_classes"][0].numpy().astype(np.uint8)[idx]),
+                "class_name": self.labels[predictions["detection_classes"][0].numpy().astype(np.uint8)[idx]],
+                "probability": float(predictions["detection_scores"][0].numpy()[idx]),
+                "bounding_box": predictions["detection_boxes"][0].numpy()[idx],
+                "mask": predictions["mask_predictions"][0].numpy()[idx]
+            })
+
+        return detections
+
+    def visualize(self, image_path, detections):
+        image = cv2.imread(image_path)
+
+        image = self.draw_bounding_boxes(image, detections)
+                                 
+        self.show_image(image)
+
+
+        return image
+
+    def draw_bounding_boxes(self, image, detections):
+        height, width, _ = image.shape
+        
+        for detection in detections:
+            # print("object found class=" + str() + ", prob=" + str(scores[idx]))
+
+            y1 = int(detection["bounding_box"][0] * height)
+            x1 = int(detection["bounding_box"][1] * width)
+            y2 = int(detection["bounding_box"][2] * height)
+            x2 = int(detection["bounding_box"][3] * width)
+
+            image = cv2.rectangle(image, (x1, y1), (x2, y2), (0,0,255), 2)
+
+            text = str(detection["class_name"]) + " " + str(round(detection["probability"]*100)) + "%"
+
+            cv2.putText(img=image,
+                        text=text,
+                        org=(x1,y1-3), 
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=1,
+                        color = (0,0,255),
+                        thickness=2,
+                        lineType=2)
+
+            mask = detection["mask"].astype(np.float32)
+            self.show_image(mask)
+
+            mask = cv2.resize(mask, (x2-x1, y2-y1)) 
+            mask = mask > -5
+            mask = mask.astype(np.uint8)
+            mask *= 255
+            self.show_image(mask)
+
+            image_box = image[y1:y2, x1:x2, :]
+
+            alpha=0.3
+            color = (0,0,1)
+            for c in range(3):
+                image_box[:, :, c] = np.where(mask == 255,
+                                    image_box[:, :, c] *
+                                    (1 - alpha) + alpha * color[c] * 255,
+                                    image_box[:, :, c])
+
+            image[y1:y2, x1:x2, :] = image_box
+
+            # TODO: image wird jedes mal überschrieben, original image beibehalten
+
+
+        return image
+
+    def draw_mask(self, image, detections):
+        return
+
+    def evaluate(self):
+        return
+
+    def show_image(self, image):
+        plt.figure()
+        plt.imshow(image)
+        plt.show()
+        # cv2.imshow("bboxes", image)
+        # cv2.waitKey(0)
 
     def infer_on_webserver(self, webserver_uri, image_path):
         image = PIL.Image.open(image_path)
@@ -90,11 +189,23 @@ def convert_from_image_to_cv2(img: PIL.Image):
 
 
 def main():
-    path = os.path.join(os.getcwd(), "dataset", "augmented_dataset", "bottle", "images")
+    type_name = "bottle"
+    images_path = os.path.join(os.getcwd(), "dataset", "augmented_dataset", type_name, "validate", "images")
+    test_image_path = os.path.join(images_path, "contamination", "013.png")
+    model_path = os.path.join(os.getcwd(), "models", "mask_rcnn", "1")
 
-    model = MaskRCNN(model_path="models/1/saved_model", only_cpu=True)
+    model = MaskRCNN(model_path=os.path.join(model_path, "saved_model"),
+                     label_path=os.path.join(model_path, "labels.json"), 
+                     type_name=type_name,
+                     only_cpu=True)
 
-    model.predict(os.path.join(path, "broken_small", "001.png"))
+    # detections = model.predict(image_path, threshold=0.5)
+
+    # np.save("./predictions.npy", detections)
+    detections_file = np.load("./predictions.npy", allow_pickle=True)
+    
+
+    model.visualize(test_image_path, detections_file)
 
     # masks = model.infer_on_webserver("http://iras-w06o:9930", os.path.join(path, "broken_small", "001.png"))
 
